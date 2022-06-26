028b524e189f26a5b77a80141b878277b16a32fc
Giulia <carmogiulia8@gmail.com>
2022-06-26 19:28:09 -0300

    decola tech desafio

---
+++ desafio decola tech/média1.js
@@ -0,0 +1,10 @@
+// a função gets é implementada dentro do sistema para ler as entradas(inputs) dos dados e a função print para imprimir a saída (output) de dados e já pula uma linha ("\n")
+// Abaixo segue um exemplo de código que você pode ou não utilizar
+
+var a = parseFloat(gets(0,0));
+var b = parseFloat(gets(0,5));
+
+//TODO: Complete os espaços em branco com uma possível solução para o desafio
+
+var media = (( 3.5 * a ) + ( 7.5 * b))/ 11 ;
+print("MEDIA = " + media.toFixed(5));
